6b916e23765363f1463c62600bcea73e37791448
fohra <atte.fohr@gmail.com>
2020-07-27 09:55:49 +0300

    Changed extract_edu to able to extract given columns

--- master/tested_with_real_data/extract_edu.py
+++ master/tested_with_real_data/extract_edu.py
@@ -5,19 +5,19 @@ Created on Fri Jun 26 12:39:40 2020
 @author: Atte
 """
 
-def extract_edu(data):
+def extract_edu(data, list_of_cols):
     #data = pandas dataframe
     #list_of_cols = list of columns to be extracted e.g. ['TNRO', 'vuosi', 'iscfi2013', 'kaste_t2']
     #
     #Returns dataframe with given columns, where rows with no year info are dropped
-    
+    ret = data[list_of_cols]
     
     #remove rows with no yearly info
-    data = data.dropna(subset=[data.columns[1]])
+    ret = ret.dropna(subset=[ret.columns[1]])
     
     #change NaNs in kaste to 91 = unknown
-    data[data.columns[3]] = data[data.columns[3]].fillna(91)
+    ret[ret.columns[3]] = ret[ret.columns[3]].fillna(91)
     #And Nans in iscfi2013 to 99
-    data[data.columns[2]] = data[data.columns[2]].fillna(99)
+    ret[ret.columns[2]] = ret[ret.columns[2]].fillna(99)
     
-    return(data)
+    return(ret)
